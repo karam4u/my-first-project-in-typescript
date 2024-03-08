5166d92c40b721430fe3adb51c93ef1408508314
karam4u <160854629+karam4u@users.noreply.github.com>
2024-03-08 05:30:55 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+// MY first project typing hello world
+var massage = "hello world";
+console.log(massage);
---
+++ main.ts
@@ -0,0 +1,4 @@
+// MY first project typing hello world
+
+let massage = "hello world"
+console.log(massage)
